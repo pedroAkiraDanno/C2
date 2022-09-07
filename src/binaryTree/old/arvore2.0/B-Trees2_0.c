da76307c61a523e42ea1ccecbc64c267febe7ae9
Pedro Akira <pedro-akira@hotmail.com>
2022-09-07 00:00:49 -0300

    feature(binaryTree) add old files about tree and binary tree

---
+++ src/binaryTree/old/arvore2.0/B-Trees2_0.c
@@ -0,0 +1,46 @@
+  /***************************************************************
+  * OBJETIVO  : ESTRUTURA-DE-DADOS-B-Trees                       *
+  * CRIACAO   : 10-02-2020	                    				         *
+  * VERSAO    : 1.0		                        				           *
+  * AUTOR     : Pedro Akira Danno Lima          				         * 
+  ***************************************************************/
+
+
+
+
+
+
+/*
+B-Trees
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+  /*****************************************************************************************************************
+  *                                                   REFERENCIAS                                                  *
+  *https://www.youtube.com/watch?v=oxTVYaKGg2A                                                                     *
+  *https://www.youtube.com/watch?v=Ay2AbTk_QEg&list=PLdo5W4Nhv31YvoZ1PjW7QRKDQn8gM6yq3&index=2                     *
+  *https://www.youtube.com/watch?v=V-6i1V_zVJQ                                                                     *
+  *https://www.youtube.com/watch?v=aZjYr87r1b8                                                                     *
+  ******************************************************************************************************************/
+
+
+
+
+
+
+
+
+
+
