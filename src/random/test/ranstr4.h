5e2d4edc4af30275d8ca7b5c98d88728ac9328ef
Pedro Akira <pedro-akira@hotmail.com>
2022-09-11 22:54:37 -0300

    improve random folder

---
+++ src/random/test/ranstr4.h
@@ -0,0 +1,39 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * not a cryptographically secure number
+ * return interger [0, n).
+ */
+int intN(int n);
+
+/**
+ * Input: length of the random string [a-z0-9] to be generated
+ */
+char *randomString(int len);
+
+
+
+
+
+
+
+/*
+COMPILE:
+
+cd /usr/include
+vi ranstr4.h
+
+
+
+
+REFE:
+https://siongui.github.io/2017/02/09/c-generate-random-string/
+
+
+*/
+
+
