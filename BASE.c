97aa4c2e6bcf797e8b5428f334879a26fc0e3871
Pedro Akira <40009054+pedroAkiraDanno@users.noreply.github.com>
2022-12-21 11:41:05 -0300

    Update BASE.c

--- BASE.c
+++ BASE.c
@@ -1,1 +1,129 @@
-#c
+
+
+ /**********************************************
+  * GOAL       : BINARY SEARCH		           
+  * CREATION   : 20-12-2022	                   
+  * VERSION    : 1.0		                       
+  * AUTHOR     : Pedro Akira Danno Lima         
+  **********************************************/
+
+
+
+
+// C program to implement iterative Binary Search
+#include <stdio.h>
+#include<stdlib.h>
+ 
+// A iterative binary search function. It returns
+// location of x in given array arr[l..r] if present,
+// otherwise -1
+int binarySearch(int arr[], int l, int r, int x)
+{
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+ 
+        // Check if x is present at mid
+        if (arr[m] == x)
+            return m;
+ 
+        // If x greater, ignore left half
+        if (arr[m] < x)
+            l = m + 1;
+ 
+        // If x is smaller, ignore right half
+        else
+            r = m - 1;
+    }
+ 
+    // if we reach here, then element was
+    // not present
+    return -1;
+}
+ 
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1) ? printf("Element is not present"
+                            " in array")
+                   : printf("Element is present at "
+                            "index %d",
+                            result);
+    return 0;
+}
+
+
+
+
+
+
+
+
+/*
+	EXEC:
+
+
+	sudo su -
+	sudo apt-get install gcc -y
+	#sudo apt-get update && sudo apt-get upgrade -y
+	#apt list --upgradable	
+
+
+	cd ~
+	rm -fr C2/
+	git clone https://github.com/pedroAkiraDanno/C2
+	cd C2/data_structure2/
+
+
+	gcc -Wall -O2 binary_search2.c -o binary_search2
+	#OR
+	gcc binary_search2.c -o binary_search2
+
+	./binary_search2
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*****************************************************************************************************************
+  *                                                   REFERENCES      	                                           
+  *
+	* 			https://www.geeksforgeeks.org/binary-search/
+	*
+	*																		   
+  ******************************************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
