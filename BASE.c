5746d19048fcac30f9aa0ec9189f300130f8290e
Pedro Akira <40009054+pedroAkiraDanno@users.noreply.github.com>
2022-12-21 11:56:25 -0300

    Update BASE.c
    base of file about C

--- BASE.c
+++ BASE.c
@@ -1,7 +1,7 @@
 
 
  /**********************************************
-  * GOAL       : BINARY SEARCH		           
+  * GOAL       : BASE		           
   * CREATION   : 20-12-2022	                   
   * VERSION    : 1.0		                       
   * AUTHOR     : Pedro Akira Danno Lima         
@@ -10,48 +10,27 @@
 
 
 
-// C program to implement iterative Binary Search
+// HEADERS
 #include <stdio.h>
 #include<stdlib.h>
  
-// A iterative binary search function. It returns
-// location of x in given array arr[l..r] if present,
-// otherwise -1
-int binarySearch(int arr[], int l, int r, int x)
+
+// FUNCITONS
+int binarySearch()
 {
-    while (l <= r) {
-        int m = l + (r - l) / 2;
- 
-        // Check if x is present at mid
-        if (arr[m] == x)
-            return m;
- 
-        // If x greater, ignore left half
-        if (arr[m] < x)
-            l = m + 1;
- 
-        // If x is smaller, ignore right half
-        else
-            r = m - 1;
-    }
- 
-    // if we reach here, then element was
-    // not present
-    return -1;
+
+    return 0;
 }
  
+
+
+// MAIN 
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 10;
-    int result = binarySearch(arr, 0, n - 1, x);
-    (result == -1) ? printf("Element is not present"
-                            " in array")
-                   : printf("Element is present at "
-                            "index %d",
-                            result);
+
+	
     return 0;
+	
 }
 
 
@@ -62,7 +41,7 @@ int main(void)
 
 
 /*
-	EXEC:
+	EXEC GIT:
 
 
 	sudo su -
@@ -74,18 +53,41 @@ int main(void)
 	cd ~
 	rm -fr C2/
 	git clone https://github.com/pedroAkiraDanno/C2
-	cd C2/data_structure2/
+	cd C2/
 
 
-	gcc -Wall -O2 binary_search2.c -o binary_search2
+	gcc -Wall -O2 BASE.c -o BASE
 	#OR
-	gcc binary_search2.c -o binary_search2
+	gcc BASE.c -o BASE
 
-	./binary_search2
-
-
+	./BASE
 
 
+*/
+
+
+
+
+
+
+
+
+/*
+
+	EXEC :
+
+	sudo su -
+	sudo apt-get install gcc -y
+	#sudo apt-get update && sudo apt-get upgrade -y
+	#apt list --upgradable	
+
+
+
+	gcc -Wall -O2 BASE.c -o BASE
+	#OR
+	gcc BASE.c -o BASE
+
+	./BASE
 
 
 */
@@ -105,9 +107,9 @@ int main(void)
   /*****************************************************************************************************************
   *                                                   REFERENCES      	                                           
   *
-	* 			https://www.geeksforgeeks.org/binary-search/
-	*
-	*																		   
+  * 	https://www.geeksforgeeks.org/binary-search/
+  *
+  *																		   
   ******************************************************************************************************************/
 
 
